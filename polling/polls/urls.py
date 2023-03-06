4ea2acdbc21ad747a39fd8f13d499c4b61133e6c
seymaozukanar <seyma.ozukanar@hipo.biz>
2023-03-06 23:21:28 +0300

    Add poll detail endpoint & improve list template

--- polling/polls/urls.py
+++ polling/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_polls
+from .views import list_polls, poll_detail
 
 urlpatterns = [
     path("", list_polls, name="list_polls"),
+    path("<int:id>/", poll_detail, name="poll_detail")
 ]
